a13346a2295cc9cd5f0456b8d32f839fb3ecb9b4
nvertigo <nvertigo67@gmail.com>
2016-12-07 12:57:05 +0100

    BoardConfig.mk: change TARGET_KERNEL_CONFIG to cyanogenmod_deathly_chagallwifi_defcomfig.
    Because cyanogenmod_chagallwifi_defconfig doesn't exist in
    github.com/exynos5420/android_kernel_samsung_exynos5420.
    
    Change-Id: I5030fe5346d35ab25d96b4d867196440bac89275

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,7 +57,7 @@ COMMON_GLOBAL_CFLAGS += -DFORCE_SCREENSHOT_CPU_PATH
 # Kernel
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_CONFIG := cyanogenmod_chagallwifi_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_deathly_chagallwifi_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos5420
 
 # Charging mode
