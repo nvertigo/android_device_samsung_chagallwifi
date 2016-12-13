2ae840e06815fd6d4a543b5818bae285b77bbebb
nvertigo <nvertigo67@gmail.com>
2016-12-13 16:14:14 +0100

    device.mk: add xxhdpi and xxxhdpi to PRODUCT_AAPT_PREBUILT_DPI.
    Change-Id: Ib08de0cd2bd55c7b11c6ab494b3e5020d70e5a18

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := xlarge
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
-PRODUCT_AAPT_PREBUILT_DPI := hdpi mdpi
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xxxhdpi hdpi mdpi
 
 # Audio
 PRODUCT_PACKAGES += \
