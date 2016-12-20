15ebced74ad78edab90cebd2b0cbcf08af86a5ae
nvertigo <nvertigo67@gmail.com>
2016-12-20 10:11:51 +0100

    audio: decommonize audio_policy.conf.
    Change-Id: I21e7ce325d9bac64588df396db40656445a3ec63

--- device.mk
+++ device.mk
@@ -29,7 +29,8 @@ PRODUCT_PACKAGES += \
     audio.primary.universal5420
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 2560
